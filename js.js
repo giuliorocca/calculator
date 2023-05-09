d0fba182ef6098fc95eff57d53f0458c05768549
Giulio <42920670+giuliorocca@users.noreply.github.com>
2023-05-09 23:05:22 +0200

    adds basic display functionality

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ let num1;
 let num2;
 let operator;
 
-// Define the basic math operations
+// Define basic math operations
 function add (num1, num2) {
     return num1 + num2;
 }
@@ -13,10 +13,10 @@ function multiply (num1, num2) {
     return num1 * num2;
 }
 function divide (num1, num2) {
-    return num1/num2;
+    return num1 / num2;
 }
 
-// Define the basic calculator functionality
+// Define basic calculator functionality
 function operate (num1, num2, operator) {
     if (operator == "+") {
         return add(num1, num2);
@@ -29,3 +29,16 @@ function operate (num1, num2, operator) {
     }
 }
 
+// Define function that updates the display 
+function updateDisplay(event) {
+    let buttonText = event.target.textContent;
+    let display = document.getElementById("display");
+    display.textContent = display.textContent + buttonText;
+    displayValue = display.textContent;
+}
+
+// Attach click event listeners to all the buttons
+const buttons = document.getElementsByClassName("button");
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", updateDisplay);
+}

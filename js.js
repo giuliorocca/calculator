046e83605be0abefd57d2b1b2c7f923d761f00d7
Giulio <42920670+giuliorocca@users.noreply.github.com>
2023-05-10 18:09:16 +0200

    adds two row display functionality WIP

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 let num1;
 let num2;
 let operator;
+const sound = new Audio('click.mp3');
 
 // Define basic math operations
 function add (num1, num2) {
@@ -29,8 +30,9 @@ function operate (num1, num2, operator) {
     }
 }
 
-// Define how calculator display updates (across two lines)
+// Define how calculator display updates (on two lines)
 function updateDisplay(event) {
+    sound.play;
     let buttonText = event.target.textContent;
     let firstLine = document.getElementById("firstline");
     let secondLine = document.getElementById("secondline");
@@ -42,7 +44,7 @@ function updateDisplay(event) {
     }
 
     // Show numbers/decimals in display when numbers/decimals buttons clicked
-    // in first line or second line of display, depending if first number input
+    // in first line or on second line display if first number already input
     if (event.target.classList.contains("number")) {
         if (firstLine.textContent === "") {
             firstLine.textContent = firstLine.textContent + buttonText;
@@ -52,21 +54,25 @@ function updateDisplay(event) {
         }
     }
 
-    // Reduce font of first number when followed by an operator
+    // Reduce font of first number when followed by an operator button
     // and save the first number and operator as variables
     if (event.target.classList.contains("operator")) {
         firstLine.textContent = firstLine.textContent + buttonText;
         firstLine.style.fontSize = "24px";
         operator = buttonText;
         num1 = parseFloat(firstLine.textContent);
+        console.log(num1);
     }
     
     // Evaluate the two inputted numbers when equals button clicked
     if (event.target.id === "equals") {
-        num2 = parseFloat(firstLine.textContent);
+        num2 = parseFloat(secondLine.textContent);
+        console.log(num2);
         let output = operate(num1, num2, operator);
+        console.log(output);
         firstLine.textContent = output;
         firstLine.style.fontSize = "30px";
+        secondLine.textContent = "";
     }
 }
 

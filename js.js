478e230a5b452e228f3e168f7e785593d4f89fa3
Giulio <42920670+giuliorocca@users.noreply.github.com>
2023-05-10 17:15:20 +0200

    adds two row display functionality WIP

--- js.js
+++ js.js
@@ -16,7 +16,7 @@ function divide (num1, num2) {
     return num1 / num2;
 }
 
-// Define basic calculator functionality
+// Define how calculator accepts operator inputs
 function operate (num1, num2, operator) {
     if (operator == "+") {
         return add(num1, num2);
@@ -29,12 +29,45 @@ function operate (num1, num2, operator) {
     }
 }
 
-// Define function that updates the display 
+// Define how calculator display updates (across two lines)
 function updateDisplay(event) {
     let buttonText = event.target.textContent;
-    let display = document.getElementById("display");
-    display.textContent = display.textContent + buttonText;
-    displayValue = display.textContent;
+    let firstLine = document.getElementById("firstline");
+    let secondLine = document.getElementById("secondline");
+
+    // Clear display when AC button clicked
+    if (event.target.id === "ac") {
+        firstLine.textContent = "";
+        secondLine.textContent = "";
+    }
+
+    // Show numbers/decimals in display when numbers/decimals buttons clicked
+    // in first line or second line of display, depending if first number input
+    if (event.target.classList.contains("number")) {
+        if (firstLine.textContent === "") {
+            firstLine.textContent = firstLine.textContent + buttonText;
+        }
+        else {
+            secondLine.textContent = secondLine.textContent + buttonText;
+        }
+    }
+
+    // Reduce font of first number when followed by an operator
+    // and save the first number and operator as variables
+    if (event.target.classList.contains("operator")) {
+        firstLine.textContent = firstLine.textContent + buttonText;
+        firstLine.style.fontSize = "24px";
+        operator = buttonText;
+        num1 = parseFloat(firstLine.textContent);
+    }
+    
+    // Evaluate the two inputted numbers when equals button clicked
+    if (event.target.id === "equals") {
+        num2 = parseFloat(firstLine.textContent);
+        let output = operate(num1, num2, operator);
+        firstLine.textContent = output;
+        firstLine.style.fontSize = "30px";
+    }
 }
 
 // Attach click event listeners to all the buttons
